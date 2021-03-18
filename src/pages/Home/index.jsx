929678716b648524d111084d4514a51805d0e0a2
MarcosRene <renemoura789@gmail.com>
2021-03-18 19:28:35 -0300

    :lipstick: chore: home page styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,80 @@
 import React from 'react';
 
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import './styles.scss';
+
 const Home = () => (
   <div className="container">
-    <div className="content">
-      <h1>Home</h1>
-    </div>
+    <ul className="list__items">
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+            1
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li className="list__items-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
+        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
+        <span>R$309,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f7fafc" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ul>
   </div>
 );
 
