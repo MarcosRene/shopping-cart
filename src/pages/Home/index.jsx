f4ab09154c6b642b42d3e436dc23bdec9a9901c2
MarcosRene <renemoura789@gmail.com>
2021-03-18 21:12:16 -0300

    :alien: chore: searching for data in the api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,81 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
+
 import './styles.scss';
 
-const Home = () => (
-  <div className="container">
-    <ul className="list__items">
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-            1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li className="list__items-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_0aP-ccYXK1oqts6QR93819kZDp2nEDjFPmX89qNDFXy0ZUoy2pC9v7CsQgDX3hhDaOTnop0&usqp=CAc" alt="Tenis Nike Revolution 5 Preto/branco" />
-        <strong>Tenis Nike Revolution 5 Preto/branco</strong>
-        <span>R$309,99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#f7fafc" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ul>
-  </div>
-);
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get('/products');
+      setProducts(data);
+    }
+
+    loadProducts();
+  }, []);
+
+  return (
+    <div className="container">
+      <ul className="list__items">
+        {products.map((product) => (
+          <li key={product.id} className="list__items-item">
+            <img
+              src={product.image}
+              alt={product.title}
+            />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#f7fafc" />
+                {product.amout || 0}
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
